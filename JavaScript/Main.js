4aaa6fb11084c456bc6d7e2c0d10275e9c294b7c
ohhyunmin <skemfdl1010@naver.com>
2020-08-15 15:06:41 +0900

    YoutubeClone_step2 - Css 를 추가하여 동적인 페이지로 변환 및 자바스크립트 이용해보는 학습

---
+++ JavaScript/Main.js
@@ -0,0 +1,8 @@
+const moreButton = document.querySelector(".info .metadata .moreButton");
+
+const title = document.querySelector(".info .metadata .title");
+
+moreButton.addEventListener("click", () => {
+  moreButton.classList.toggle("clicked");
+  title.classList.toggle("clamp");
+});
